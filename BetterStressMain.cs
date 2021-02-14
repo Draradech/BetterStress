64d33472efbffb86b2e883a74d951238b52a296a
draradech <kasten.m@gmx.de>
2021-02-14 16:33:26 +0100

    cleanup, fix hotkey handling

--- BetterStressMain.cs
+++ BetterStressMain.cs
@@ -1,11 +1,11 @@
-﻿using System;
-using System.Reflection;
-using System.Collections.Generic;
-using UnityEngine;
-using BepInEx;
+﻿using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
 using PolyTechFramework;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
 
 namespace BetterStress
 {
@@ -14,10 +14,10 @@ namespace BetterStress
     [BepInDependency(ConfigurationManager.ConfigurationManager.GUID, BepInDependency.DependencyFlags.HardDependency)]
     public class BetterStressMain : PolyTechMod
     {
-        new public const String PluginGuid = "draradech.pb2plugins.BetterStress";
-        new public const String PluginName = "Better Stress";
-        new public const String PluginVersion = "0.9.5";
-        
+        public new const String PluginGuid = "draradech.pb2plugins.BetterStress";
+        public new const String PluginName = "Better Stress";
+        public new const String PluginVersion = "0.9.5";
+
         private static ConfigEntry<bool> modEnabled;
         private static ConfigEntry<bool> replayStress;
         private static ConfigEntry<Vector3> colCompressionMin;
@@ -29,7 +29,7 @@ namespace BetterStress
         private static ConfigEntry<float>[] stressExponent;
         private static ConfigEntry<BepInEx.Configuration.KeyboardShortcut> stressExponentKey;
         private static Dictionary<ConfigEntryBase, ColorCacheEntry> _colorCache = new Dictionary<ConfigEntryBase, ColorCacheEntry>();
-        
+
         private static BepInEx.Logging.ManualLogSource staticLogger;
         private static Dictionary<Material, Color> originalColor = new Dictionary<Material, Color>();
         private static Dictionary<PolyPhysics.Edge, float> maxStress = new Dictionary<PolyPhysics.Edge, float>();
@@ -37,11 +37,11 @@ namespace BetterStress
         private static bool maxStressSelected = false;
         private static int selectedExponent = 1;
         private static BridgeEdge newEdge;
-        
+
         void Awake()
         {
             stressExponent = new ConfigEntry<float>[3];
-            
+
             modEnabled        = Config.Bind("", "Mod Enabled",                        true,                                                   new ConfigDescription("", null, new ConfigurationManagerAttributes{Order = 12}));
             replayStress      = Config.Bind("", "Replay records stress view",         false,                                                  new ConfigDescription("", null, new ConfigurationManagerAttributes{Order = 11}));
             stressSmoothing   = Config.Bind("", "Current stress smoothing",           0.8f,                                                   new ConfigDescription("", null, new ConfigurationManagerAttributes{Order = 10}));
@@ -54,96 +54,96 @@ namespace BetterStress
             colCompressionMin = Config.Bind("", "Compression Min",                    new Vector3(0.0f, 1.0f, 0.0f),                          new ConfigDescription("", null, new ConfigurationManagerAttributes{Order =  3, CustomDrawer = HsvDrawer}));
             colTensionMin     = Config.Bind("", "Tension Min",                        new Vector3(0.5f, 1.0f, 0.0f),                          new ConfigDescription("", null, new ConfigurationManagerAttributes{Order =  2, CustomDrawer = HsvDrawer}));
             colTensionMax     = Config.Bind("", "Tension Max",                        new Vector3(0.5f, 1.0f, 0.8f),                          new ConfigDescription("", null, new ConfigurationManagerAttributes{Order =  1, CustomDrawer = HsvDrawer}));
-            
+
             modEnabled.SettingChanged += onEnableDisable;
-            
+
             staticLogger = Logger;
-            
+
             var harmony = new Harmony(PluginGuid);
             harmony.PatchAll(Assembly.GetExecutingAssembly());
-            
+
             this.isCheat = false;
             this.isEnabled = true;
             PolyTechMain.registerMod(this);
         }
-        
+
         private sealed class ColorCacheEntry
         {
-          public Vector3 Last;
-          public Texture2D Tex;
+            public Vector3 Last;
+            public Texture2D Tex;
         }
-        
+
         private static void HsvDrawer(ConfigEntryBase entry)
         {
             var vec = (Vector3)entry.BoxedValue;
-            
+
             if (!_colorCache.TryGetValue(entry, out var cacheEntry))
             {
                 cacheEntry = new ColorCacheEntry { Tex = new Texture2D(40, 60, TextureFormat.ARGB32, false), Last = vec };
                 cacheEntry.Tex.FillTexture(Color.HSVToRGB(vec.x, vec.y, vec.z));
                 _colorCache[entry] = cacheEntry;
             }
-            
+
             GUILayout.BeginVertical(GUILayout.ExpandWidth(true));
-            
+
             GUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
             GUILayout.Label("H", GUILayout.ExpandWidth(false));
             vec.x = Mathf.Round(100.0f * GUILayout.HorizontalSlider(vec.x, 0f, 1f, GUILayout.ExpandWidth(true))) / 100.0f;
             GUILayout.Label($"{vec.x:F2}", GUILayout.ExpandWidth(false));
             GUILayout.EndHorizontal();
-            
+
             GUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
             GUILayout.Label("S", GUILayout.ExpandWidth(false));
             vec.y = Mathf.Round(100.0f * GUILayout.HorizontalSlider(vec.y, 0f, 1f, GUILayout.ExpandWidth(true))) / 100.0f;
             GUILayout.Label($"{vec.y:F2}", GUILayout.ExpandWidth(false));
             GUILayout.EndHorizontal();
-            
+
             GUILayout.BeginHorizontal(GUILayout.ExpandWidth(true));
             GUILayout.Label("V", GUILayout.ExpandWidth(false));
             vec.z = Mathf.Round(100.0f * GUILayout.HorizontalSlider(vec.z, 0f, 1f, GUILayout.ExpandWidth(true))) / 100.0f;
             GUILayout.Label($"{vec.z:F2}", GUILayout.ExpandWidth(false));
             GUILayout.EndHorizontal();
-            
+
             GUILayout.EndVertical();
-            
+
             if (vec != cacheEntry.Last)
             {
                 entry.BoxedValue = vec;
                 cacheEntry.Tex.FillTexture(Color.HSVToRGB(vec.x, vec.y, vec.z));
                 cacheEntry.Last = vec;
             }
-            
+
             GUILayout.Label(cacheEntry.Tex, GUILayout.ExpandWidth(false));
         }
-        
+
         private void onEnableDisable(object sender, EventArgs e)
         {
             if (modEnabled.Value) enableMod();
             else disableMod();
         }
-        
+
         public override void enableMod()
         {
             this.isEnabled = true;
             modEnabled.Value = true;
         }
-        
+
         public override void disableMod()
         {
             SetOriginalColor();
             this.isEnabled = false;
             modEnabled.Value = false;
         }
-        
+
         public override string getSettings()
         {
             return "";
         }
-        
+
         public override void setSettings(string settings)
         {
         }
-        
+
         private static void SetColor(Material m, Color c)
         {
             if (!originalColor.ContainsKey(m))
@@ -169,29 +169,39 @@ namespace BetterStress
             }
         }
 
+        [HarmonyPatch(typeof(MainCamera), "OnPreRender")]
+        static class Patch_MainCamera_OnPreRender
+        {
+            [HarmonyPostfix]
+            static void Postfix()
+            {
+                if (!modEnabled.Value) return;
+
+                if (maxStressHotkey.Value.IsDown())
+                {
+                    maxStressSelected = !maxStressSelected;
+                }
+
+                if (stressExponentKey.Value.IsDown())
+                {
+                    selectedExponent = (selectedExponent + 1) % 3;
+                }
+            }
+        }
+
         [HarmonyPatch(typeof(BridgeEdges), "SetStressColor")]
         static class Patch_BridgeEdges_SetStressColor
         {
             [HarmonyPostfix]
             static void Postfix()
             {
-                if(!modEnabled.Value) return;
-                
-                if (maxStressHotkey.Value.IsDown())
-                {
-                    maxStressSelected = !maxStressSelected;
-                }
-                
-                if(stressExponentKey.Value.IsDown())
-                {
-                    selectedExponent = (selectedExponent + 1) % 3;
-                }
-                
+                if (!modEnabled.Value) return;
+
                 var bridgeLinkMeshRendererField = typeof(BridgeLink).GetField("m_MeshRenderer", BindingFlags.NonPublic | BindingFlags.Instance);
                 foreach (BridgeEdge edge in BridgeEdges.m_Edges)
                 {
                     float stress = 0.0f;
-                    if(edge.m_PhysicsEdge != null)
+                    if (edge.m_PhysicsEdge != null)
                     {
                         stress = maxStressSelected ? maxStress[edge.m_PhysicsEdge] : curStress[edge.m_PhysicsEdge];
                     }
@@ -200,7 +210,8 @@ namespace BetterStress
                     stress = Mathf.Pow(Mathf.Abs(stress), stressExponent[selectedExponent].Value);
                     Vector3 hsv = compression ? Vector3.Lerp(colCompressionMin.Value, colCompressionMax.Value, stress) : Vector3.Lerp(colTensionMin.Value, colTensionMax.Value, stress);
                     Color stressCol = Color.HSVToRGB(hsv.x, hsv.y, hsv.z);
-                    if ((edge.m_Material.m_MaterialType == BridgeMaterialType.ROPE || edge.m_Material.m_MaterialType == BridgeMaterialType.CABLE) && BridgeRopes.m_BridgeRopes.Count > 0)
+
+                    if (edge.m_Material.m_MaterialType == BridgeMaterialType.ROPE || edge.m_Material.m_MaterialType == BridgeMaterialType.CABLE)
                     {
                         foreach (BridgeRope bridgeRope in BridgeRopes.m_BridgeRopes)
                         {
@@ -210,12 +221,11 @@ namespace BetterStress
                                 foreach (BridgeLink link in bridgeRope.m_Links)
                                 {
                                     SetColor(((MeshRenderer)bridgeLinkMeshRendererField.GetValue(link)).material, stressCol);
-                                    
                                 }
                             }
                         }
                     }
-                    else if (edge.m_Material.m_MaterialType == BridgeMaterialType.SPRING && BridgeSprings.m_BridgeSprings.Count > 0)
+                    else if (edge.m_Material.m_MaterialType == BridgeMaterialType.SPRING)
                     {
                         BridgeSprings.SetStressColorForEdge(edge, 0.0f);
                         SetColor(edge.m_SpringCoilVisualization.m_FrontLink.m_MeshRenderer.material, stressCol);
@@ -230,7 +240,7 @@ namespace BetterStress
                             SetColor(edge.m_MeshRenderer.materials[1], stressCol);
                         }
                     }
-                    
+
                     if (edge.m_HydraulicEdgeVisualization != null)
                     {
                         edge.m_HydraulicEdgeVisualization.SetStressColorForEdge(edge, 0.0f);
@@ -242,7 +252,7 @@ namespace BetterStress
                 }
             }
         }
-        
+
         private static void RestoreColor(Material m)
         {
             if (originalColor.ContainsKey(m))
@@ -250,25 +260,25 @@ namespace BetterStress
                 m.color = originalColor[m];
             }
         }
-        
+
         [HarmonyPatch(typeof(BridgeEdges), "SetOriginalColor")]
         static class Patch_BridgeEdges_SetOriginalColor
         {
             [HarmonyPostfix]
             static void Postfix()
             {
-                if(!modEnabled.Value) return;
-                
+                if (!modEnabled.Value) return;
+
                 SetOriginalColor();
             }
         }
-        
+
         private static void SetOriginalColor()
         {
             var bridgeLinkMeshRendererField = typeof(BridgeLink).GetField("m_MeshRenderer", BindingFlags.NonPublic | BindingFlags.Instance);
             foreach (BridgeEdge edge in BridgeEdges.m_Edges)
             {
-                if ((edge.m_Material.m_MaterialType == BridgeMaterialType.ROPE || edge.m_Material.m_MaterialType == BridgeMaterialType.CABLE) && BridgeRopes.m_BridgeRopes.Count > 0)
+                if (edge.m_Material.m_MaterialType == BridgeMaterialType.ROPE || edge.m_Material.m_MaterialType == BridgeMaterialType.CABLE)
                 {
                     foreach (BridgeRope bridgeRope in BridgeRopes.m_BridgeRopes)
                     {
@@ -281,7 +291,7 @@ namespace BetterStress
                         }
                     }
                 }
-                else if (edge.m_Material.m_MaterialType == BridgeMaterialType.SPRING && BridgeSprings.m_BridgeSprings.Count > 0)
+                else if (edge.m_Material.m_MaterialType == BridgeMaterialType.SPRING)
                 {
                     RestoreColor(edge.m_SpringCoilVisualization.m_FrontLink.m_MeshRenderer.material);
                     RestoreColor(edge.m_SpringCoilVisualization.m_BackLink.m_MeshRenderer.material);
@@ -294,7 +304,7 @@ namespace BetterStress
                         RestoreColor(edge.m_MeshRenderer.materials[1]);
                     }
                 }
-                
+
                 if (edge.m_HydraulicEdgeVisualization != null)
                 {
                     foreach (MeshRenderer meshRenderer in edge.m_HydraulicEdgeVisualization.GetComponentsInChildren<MeshRenderer>())
@@ -304,39 +314,39 @@ namespace BetterStress
                 }
             }
         }
-        
-        private static void SetDebrisColor(Material mn, Material mb)
+
+        private static void SetDebrisColor(Material matNew, Material matBroken)
         {
-            if (!originalColor.ContainsKey(mn))
+            if (!originalColor.ContainsKey(matNew))
             {
-                if (originalColor.ContainsKey(mb))
+                if (originalColor.ContainsKey(matBroken))
                 {
-                    originalColor[mn] = originalColor[mb];
+                    originalColor[matNew] = originalColor[matBroken];
                 }
             }
         }
-        
+
         [HarmonyPatch(typeof(PolyPhysics.BridgeEdgeListener), "CreateBridgeEdgeFromEdge")]
         static class Patch_BridgeEdgeListener_CreateBridgeEdgeFromEdge
         {
             [HarmonyPostfix]
             static void Postfix(ref BridgeEdge __result)
             {
-                if(!modEnabled.Value) return;
-                
+                if (!modEnabled.Value) return;
+
                 newEdge = __result;
             }
         }
-        
+
         [HarmonyPatch(typeof(PolyPhysics.BridgeEdgeListener), "CreateDebris")]
-        static class Patch_BridgeEdgeListener_CreateDebris 
+        static class Patch_BridgeEdgeListener_CreateDebris
         {
             [HarmonyPostfix]
             static void Postfix(ref Poly.Physics.EdgeHandle e, ref BridgeEdge brokenEdge)
             {
-                if(!modEnabled.Value) return;
-                
-                if(!newEdge)
+                if (!modEnabled.Value) return;
+
+                if (!newEdge)
                 {
                     staticLogger.LogError("no new edge set in create debris");
                     return;
@@ -353,9 +363,11 @@ namespace BetterStress
                         SetDebrisColor(meshRenderer.material, brokenEdge.m_HydraulicEdgeVisualization.GetComponentsInChildren<MeshRenderer>()[0].material);
                     }
                 }
+
+                newEdge = null;
             }
         }
-        
+
         [HarmonyPatch(typeof(GameStateSim), "Exit")]
         static class Patch_GameStateSim_Exit
         {
@@ -367,18 +379,18 @@ namespace BetterStress
                 curStress.Clear();
             }
         }
-        
+
         [HarmonyPatch(typeof(PolyPhysics.World), "FixedUpdate_Manual")]
         static class Patch_World_FixedUpdate_Manual
         {
             [HarmonyPostfix]
             static void Postfix()
             {
-                if(!modEnabled.Value) return;
-                
+                if (!modEnabled.Value) return;
+
                 foreach (BridgeEdge edge in BridgeEdges.m_Edges)
                 {
-                    if(edge.m_PhysicsEdge != null)
+                    if (edge.m_PhysicsEdge != null)
                     {
                         float signedStress = edge.m_PhysicsEdge.handle.stressNormalizedSigned;
                         if (!maxStress.ContainsKey(edge.m_PhysicsEdge))
@@ -399,17 +411,17 @@ namespace BetterStress
             }
         }
     }
-    
+
     internal static class Utils
     {
         public static void FillTexture(this Texture2D tex, Color color)
         {
-          for (int x = 0; x < tex.width; ++x)
-          {
-            for (int y = 0; y < tex.height; ++y)
-              tex.SetPixel(x, y, color);
-          }
-          tex.Apply(false);
+            for (int x = 0; x < tex.width; ++x)
+            {
+                for (int y = 0; y < tex.height; ++y)
+                    tex.SetPixel(x, y, color);
+            }
+            tex.Apply(false);
         }
     }
 }
